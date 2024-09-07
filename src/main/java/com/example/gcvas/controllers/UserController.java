e4aeb95baa4aad5cfab459e48912e3eb51f2e469
jonas <godoijonas16@gmail.com>
2024-09-07 18:21:44 -0300

    Create Repositorys

--- src/main/java/com/example/gcvas/controllers/UserController.java
+++ src/main/java/com/example/gcvas/controllers/UserController.java
@@ -2,33 +2,23 @@ package com.example.gcvas.controllers;
 
 import java.net.URI;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.gcvas.models.User;
 import com.example.gcvas.service.UserService;
 
 import jakarta.validation.Valid;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import com.example.gcvas.models.User;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.bind.annotation.PutMapping;
-
-
-
-
-
-
 
 @RestController
 @RequestMapping("/user")
